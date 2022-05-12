f84f39ffb9d468eb431398d4325632496ab62741
JEH <han880330@yahoo.co.jp>
2022-05-12 16:20:33 +0900

    watch属性のrelative定義でのデータ分析

--- src/component/Demo.vue
+++ src/component/Demo.vue
@@ -3,28 +3,58 @@
  <button @click="sum++">click me add 1</button>
 <h2>now imfo:{{msg}}</h2>
 <button @click="msg+='!'">change info</button>
+<h2>name:{{person.name}}</h2>
+<h2>age:{{person.age}}</h2>
+<h2>salary:{{person.job.j1.salary}}</h2>
+<button @click="person.name+='!'">change name</button>
+<button @click="person.age++">add</button>
+<button @click="person.job.j1.salary++">add</button>
 </template>
 
 <script>
 // import {h} from 'vue'
-import {ref,watch} from 'vue'
+import {ref,reactive,watch} from 'vue'
 export default {
   name: 'Demo',
   setup(){
     let sum =ref(0)
     let msg = ref('hello')
+    let person = reactive({
+      name:'zhangsan',
+      age:18,
+      job:{
+        j1:{
+          salary:20
+        }
+      }
+    })
     // case one
     // watch(sum,(newValue,oldValue)=>{
     //   console.log('sum changed',newValue,oldValue)
     // })
 
-    watch([sum,msg],(newValue,oldValue)=>{
-      console.log('sum or msg changed',newValue,oldValue)},
-      {immediate : true }
-    )
+    // 
+    
+    // watch(person,(newValue,oldValue)=>{
+    //   console.log(newValue,oldValue)
+    // })
+
+    // watch(()=>person.age,(newValue,oldValue)=>{
+    //   console.log(newValue,oldValue)
+    // })
+
+    //   watch([()=>person.age,()=>person.name],(newValue,oldValue)=>{
+    //   console.log(newValue,oldValue)
+    // })
+
+    watch(()=>person.job,(newValue,oldValue)=>{
+      console.log(newValue,oldValue)}
+      ,{deep:true})
+      
     return{
      sum,
-     msg
+     msg,
+     person
     }
   }
 }
